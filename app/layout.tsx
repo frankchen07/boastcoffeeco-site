01d927b14ca539663b4af01723e1847bb58973f3
Frank Chen <logins+gh@frank-chen.com>
2026-07-12 13:41:13 -0700

    first set of ada changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,8 +46,14 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <CartProvider>
+          <a
+            href="#main-content"
+            className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:bg-white focus:text-espresso focus:px-4 focus:py-2 focus:rounded focus:shadow-lg focus:text-sm focus:font-semibold"
+          >
+            Skip to main content
+          </a>
           <Header />
-          <main className="flex-1">{children}</main>
+          <main id="main-content" className="flex-1">{children}</main>
           <Footer />
           <CartDrawer />
         </CartProvider>
